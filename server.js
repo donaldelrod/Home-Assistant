cf27d6345f20913ded14a963f001f4b338f06cff
Donald Elrod <donald.elrod.96@gmail.com>
2018-11-21 11:46:42 -0600

    committing before starting electron and angular integration

--- server.js
+++ server.js
@@ -102,120 +102,14 @@ file_tools.readJSONFile(devicesPath).then(function(deviceList) {
     })
 });
 
-
-
-// /**
-//  * Create an OAuth2 client with the given credentials, and then execute the
-//  * given callback function.
-//  * @param {Object} credentials The authorization client credentials.
-//  * @param {function} callback The callback to call with the authorized client.
-//  */
-// function authorize(credentials, details, callback) {
-//     const {client_secret, client_id, redirect_uris} = credentials.installed;
-//     gcal_oauth = new google.auth.OAuth2(
-//         client_id, client_secret, redirect_uris[0]);
-  
-//     // Check if we have previously stored a token.
-//     fs.readFile(details.token_path, (err, token) => {
-//         if (err) return getAccessToken(gcal_oauth, details, callback);
-//         gcal_oauth.setCredentials(JSON.parse(token));
-//         callback(gcal_oauth);
-//       });
-//   }
-
-
-// /**
-//  * Get and store new token after prompting for user authorization, and then
-//  * execute the given callback with the authorized OAuth2 client.
-//  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-//  * @param {getEventsCallback} callback The callback for the authorized client.
-//  */
-// function getAccessToken(oAuth2Client, details, callback) {
-//     const authUrl = oAuth2Client.generateAuthUrl({
-//       access_type: 'offline',
-//       scope: details.scopes,
-//     });
-//     open(authUrl, function(err) {
-//         if (err) console.log(err);
-//     });
-//     console.log('Authorize this app by visiting this url:', authUrl);
-//     const rl = readline.createInterface({
-//       input: process.stdin,
-//       output: process.stdout,
-//     });
-//     rl.question('Enter the code from that page here: ', (code) => {
-//       rl.close();
-//       oAuth2Client.getToken(code, (err, token) => {
-//         if (err) return console.error('Error retrieving access token', err);
-//         oAuth2Client.setCredentials(token);
-//         // Store the token to disk for later program executions
-//         fs.writeFile(details.token_path, JSON.stringify(token), (err) => {
-//             if (err) console.error(err);
-//             console.log('Token stored to', details.token_path);
-//         });
-//         callback(oAuth2Client);
-//       });
-//     });
-//   }
-
-
-//   /**
-//  * Lists the next 10 events on the user's primary calendar.
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// function getGCalEvents(auth, numEvents) {
-//     const calendar = google.calendar({version: 'v3', auth});
-//     //var eventData = [];
-//     return new Promise(function (resolve, reject) {
-//         calendar.events.list({
-//             calendarId: 'primary',
-//             timeMin: (new Date()).toISOString(),
-//             maxResults: numEvents,
-//             singleEvents: true,
-//             orderBy: 'startTime',
-//         }, (err, res) => {
-//             if (err)
-//                 reject('The API returned an error: ' + err);
-//             const events = res.data.items;
-//             if (events.length)
-//                 resolve(events);
-//             else {
-//               reject('No upcoming events found.');
-//               console.log('No upcoming events found.');
-//             }
-//         });
-//     });
-// }
-
-// /**
-//  * Lists the labels in the user's account.
-//  *
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// function getGmailLabels(auth) {
-//     const gmail = google.gmail({version: 'v1', auth});
-//     gmail.users.labels.list({
-//       userId: 'me',
-//     }, (err, res) => {
-//       if (err) return console.log('The API returned an error: ' + err);
-//       const labels = res.data.labels;
-//       if (labels.length) {
-//         console.log('Labels:');
-//         labels.forEach((label) => {
-//           console.log(`- ${label.name}`);
-//         });
-//       } else {
-//         console.log('No labels found.');
-//       }
-//     });
-//   }
-
 var secureServer = https.createServer(options, app).listen(9876);
 
 app.route('/api/devices/:deviceID/info').get((req, res) => {
     var index = parseInt(req.params.deviceID);
     var device = devices[index].getSysInfo().then(function (deviceInfo) {
         res.json(deviceInfo);
+    }).catch(function (reason) {
+        res.send(reason);
     });
 });
 
@@ -225,7 +119,7 @@ app.route('/api/devices/:deviceID/set/:state').get((req, res) => {
     var device = devices[index];
     if (device === undefined) {
         res.send('requested device doesn\'t exist!');
-        return
+        return;
     }
     var state = req.params.state == 'on' ? true : (req.params.state == 'off' ? false : undefined);
     state = device_tools.setDeviceState(device, state);
@@ -236,13 +130,9 @@ app.route('/oauth2/google').get((req, res) => {
     var token_code = req.query.code;
     var scope_oauth = req.query.scope;
     res.send(google_tools.saveAccessToken(google_oauth, token_code));
-    // res.json({
-    //     token: token_code,
-    //     scope: scope_oauth
-    // });
 });
 
-app.route('/api/gcal/upcoming').get((req, res) => {
+app.route('/api/modules/google/cal/upcoming').get((req, res) => {
     google_tools.getGCalEvents(google_oauth, 15).then(function (events) {
         res.json(events);
     }).catch(function (reason) {
@@ -250,6 +140,6 @@ app.route('/api/gcal/upcoming').get((req, res) => {
     });
 });
 
-app.route('/api/gmail/recent').get((req, res) => {
+app.route('/api/modules/google/gmail/labels').get((req, res) => {
     google_tools.getGmailLabels(google_oauth);
 })
