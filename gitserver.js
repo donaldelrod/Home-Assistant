9fbd5a692eb8a5b889f6cc26f6c47d923afd5f71
Donald Elrod <donald.elrod.96@gmail.com>
2019-02-22 02:00:42 -0600

    added a github watcher

---
+++ gitserver.js
@@ -0,0 +1,39 @@
+'use strict';
+var http = require('http');
+var express = require('express');
+var app = express();
+const { spawn } = require('child_process')
+
+
+var server = http.createServer(app).listen(9877);
+
+let shell = null;
+
+app.route('/git').post( (req, res) => {
+
+    console.log('git push detected...');
+
+    if (shell !== null) {
+        console.log('server currently running, attempting to kil...');
+        try {
+            shell.kill();
+            console.log('server process killed successfully!');
+        } catch (err) {
+            console.log('error killing process, aborting...');
+            process.exit(1);
+        }
+    }
+
+    shell = spawn('npm build-n-run');
+
+    shell.on('exit', function (code, signal) {
+        console.log(`server.js exited with code ${code} and signal ${signal}`);
+    });
+
+    shell.stdout.on('data', (data) => {
+        console.log(`child stdout:\n${data}`);
+    });
+
+    process.stdin.pipe(shell.stdin)
+
+});
