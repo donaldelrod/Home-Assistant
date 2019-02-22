baf8bba6519ada6537a034639101c86f06969a42
Donald Elrod <donald.elrod.96@gmail.com>
2019-02-22 02:06:28 -0600

    changed to exec

--- gitserver.js
+++ gitserver.js
@@ -26,7 +26,7 @@ app.route('/git').post( (req, res) => {
         }
     }
 
-    shell = spawn('npm build-n-run');
+    shell = exec('npm build-n-run');
 
     shell.on('exit', function (code, signal) {
         console.log(`server.js exited with code ${code} and signal ${signal}`);
@@ -36,6 +36,6 @@ app.route('/git').post( (req, res) => {
         console.log(`child stdout:\n${data}`);
     });
 
-    process.stdin.pipe(shell.stdin)
+    //process.stdin.pipe(shell.stdin)
 
 });
